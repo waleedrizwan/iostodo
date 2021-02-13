fc6c1c0e9944ce0f88a276a2788f6bb7933e1dc0
Waleed Rizwan <w2rizwan@edu.uwaterloo.ca>
2021-02-12 22:57:19 -0500

    added files

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Ex. Take a shit"
+          style={{
+            width: 200,
+            borderBottomColor: "black",
+            borderWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title="Add Task" />
+      </View>
     </View>
   );
 }
@@ -14,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
